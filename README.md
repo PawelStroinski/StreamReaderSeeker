77fdd993bea3eb2a83d5b868e24c178228fc5354
Paweł Stroiński <pol84@live.com>
2014-03-19 23:52:05 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 StreamReaderSeeker
 ==================
+Two extension methods of `StreamReader` allowing to `GetPosition` and then to `Seek` back to that position. Furthermore, position can be fine-tuned by number of characters with help of `Position.ShiftByCharacters` method, if needed.
+
+### Example
+
+    var fromOneToTenInArmenian = "մեկ երկու երեք չորս հինգ վեց յոթ ութ ինը տասը";
+    var stream = new MemoryStream(Encoding.UTF8.GetBytes(fromOneToTenInArmenian));
+    var reader = new StreamReader(stream);
+    var buffer = new char[5];
+    reader.Read(buffer, 0, buffer.Length);
+    var positionAfterReadingFiveChars = reader.GetPosition();
+    var positionAfterReadingThreeChars = positionAfterReadingFiveChars.ShiftByCharacters(-2);
+    reader.Seek(positionAfterReadingThreeChars);
+    reader.Read(buffer, 0, buffer.Length);
+    var expected = fromOneToTenInArmenian.Substring(3, buffer.Length);
+    var actual = new string(buffer);
+    System.Diagnostics.Contracts.Contract.Assert(expected == actual);
+
+There is also too long example in Program.cs.
+
+### How it works
+
+When you invoke `GetPosition`, position is stored as pair of absolute byte position (cannot be amended) and relative character position (can be amended). This character position is necessary because `StreamReader` reads data in chunks decoded to series of characters, not byte-by-byte. So position needs to reflect that.
+
+When you `Seek` to position, this first sets stream position. This is easy. But how to set character position? Well, this is easy too. It just reads that number of characters. So you want to seek by relatively small number of characters but you can seek by infinitely large number of bytes. If you are not using `ShiftByCharacters` you don't have to worry about it, because character position returned from `GetPosition` is never bigger than `StreamReader`'s buffer what is by default not terribly large number.
+
+It works with Unicode also when used encoding allows multiple representations of the same source string.
+
+Don't try to use this with other TextReaders. It wouldn't work.
+
+### Caveat
+
+I only use it in hobby project. So test yourself before you put it into something large.
---
+++ StreamReaderSeeker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+public static class StreamReaderSeeker
+{
+    const int maximumBytesPerCodeUnit = 5;
+
+    public static Position GetPosition(this StreamReader reader)
+    {
+        int byteBufferSize = GetField(reader, "byteLen");
+        int characterPosition = GetField(reader, "charPos");
+        return new Position(streamPosition: reader.BaseStream.Position - byteBufferSize,
+            characterPosition: characterPosition);
+    }
+
+    public static void Seek(this StreamReader reader, Position position)
+    {
+        position.Seek(reader);
+    }
+
+    static int GetField(object instance, string field)
+    {
+        return (int)instance.GetType().InvokeMember(field,
+            System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.NonPublic
+            | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.GetField,
+            null, instance, null);
+    }
+
+    static void Seek(this StreamReader reader, long streamPosition, int characterPosition)
+    {
+        reader.DiscardBufferedData();
+        reader.BaseStream.Position = streamPosition;
+        if (characterPosition > 0)
+        {
+            var buffer = new char[characterPosition];
+            reader.Read(buffer, 0, characterPosition);
+        }
+        else
+            if (characterPosition < 0)
+            {
+                var bufferSizeInBytes = Math.Abs(characterPosition) * maximumBytesPerCodeUnit;
+                if (bufferSizeInBytes > streamPosition)
+                    bufferSizeInBytes = (int)streamPosition;
+                reader.BaseStream.Position = streamPosition - bufferSizeInBytes;
+                var buffer = new char[bufferSizeInBytes];
+                var bufferSizeInCharacters = reader.Read(buffer, 0, bufferSizeInBytes);
+                var positiveCharacterPosition = bufferSizeInCharacters - Math.Abs(characterPosition);
+                reader.BaseStream.Position = streamPosition - bufferSizeInBytes;
+                reader.Read(buffer, 0, positiveCharacterPosition);
+            }
+    }
+
+    public class Position
+    {
+        readonly long streamPosition;
+        readonly int characterPosition;
+
+        public Position(long streamPosition, int characterPosition)
+        {
+            this.streamPosition = streamPosition;
+            this.characterPosition = characterPosition;
+        }
+
+        public void Seek(StreamReader reader)
+        {
+            reader.Seek(streamPosition: streamPosition, characterPosition: characterPosition);
+        }
+
+        public Position ShiftByCharacters(int characters)
+        {
+            return new Position(streamPosition: streamPosition, characterPosition: characterPosition + characters);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("streamPosition={0}, characterPosition={1}", streamPosition, characterPosition);
+        }
+    }
+}
